ce292bfe417ae79c6af6f9c4490705b111584e0e
jiangteng <1348746268@qq.com>
2023-02-15 14:45:37 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<AppLayout />
+</template>
+
+<script>
+import AppLayout from './views/AppLayout.vue';
+export default {
+	data() {
+		return {
+
+		}
+	},
+	components: {
+		AppLayout
+	}
+}
+</script>
+
+<style>
+html, body {
+	padding: 0;
+	margin: 0;
+	height: 100vh;
+}
+#app {
+	height: 100%;
+}
+</style>
---
+++ src/views/AppLayout.vue
@@ -0,0 +1,77 @@
+<template>
+    <el-container class="page_container">
+        <el-aside class="page_aside">
+            <div class="logo_area" @click="redirectHome">
+                <img src="../assets/logo.png" style="width: 32px; height: 32px;" alt />
+                <span v-show="!isCollapse">未来出行星座测运控系统</span>
+            </div>
+            <div class="meun_area">
+                <el-menu background-color="#033864" text-color="#fff" active-text-color="#ffd04b" :collapse="isCollapse"
+                    class="el-menu-vertical-demo" router :default-active="$route.path" :unique-opened="true">
+                    <el-sub-menu v-for="(item) in menuData" :index="item.index" :key="item.index">
+                        <template #title>
+                            <el-icon>
+                                <location />
+                            </el-icon>
+                            <span>{{ item.title }}</span>
+                        </template>
+
+                        <el-menu-item v-for="(subItem) in item.subMenu" :index="`/${item.index}/${subItem.index}`"
+                            :key="subItem.index">{{ subItem.title }}</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </div>
+        </el-aside>
+        <el-container>
+            <el-header class="page_header">
+                <el-icon class="fold_icon" :size="26" color="#ffffff" @click="isCollapse = !isCollapse"
+                    :class="isCollapse ? 'fold_icon_rotate' : ''">
+                    <fold />
+                </el-icon>
+            </el-header>
+            <el-main style="padding: 0;">
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+/* eslint-disable */
+import './AppLayout.scss'
+
+export default {
+    name: 'AppLayout',
+    data() {
+        return {
+            isCollapse: window.screen.width <= 1440 ? true : false,
+            menuData: [
+                {
+                    title: '用户管理',
+                    index: 'dataMonitoring',
+                    subMenu: [
+                        {
+                            title: '卫星跟踪',
+                            index: 'sateTrack'
+                        }
+                    ]
+                },
+                {
+                    title: '角色管理',
+                    index: 'dataMonitoring',
+                    subMenu: [
+                        
+                    ]
+                }
+            ]
+        }
+    },
+
+    methods: {
+
+        redirectHome() {
+            this.$router.push("/");
+        }
+    },
+}
+</script>
